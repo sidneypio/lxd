3279bf9c7b502b29af31796980cf86ecb61cee54
Sidney Pio de Campos <sidneypio@gmail.com>
2019-05-01 17:47:54 -0300

    nginx instalado

--- nginx/remove-ambiente.sh
+++ nginx/remove-ambiente.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 # remove o primeiro ambiente:
 # as duas maquinas e a rede
-echo "Parando a primeira maquina"
-lxc exec d1 -- /sbin/poweroff
+echo "Parando a maquina n1"
+lxc exec n1 -- /sbin/poweroff
 
-echo "Parando a segunda maquina"
-lxc exec d2 -- /sbin/poweroff
+echo "Removendo a  maquina  n1"
+lxc delete n1  
 
-echo "Removendo a primeira maquina"
-lxc delete d1  
-
-echo "Removendo a segunda maquina"
-lxc delete d2 
-
-echo "Removendo a  rede reded1d2"
-lxc network delete reded1d2 
+echo "Removendo a o redirecionamento"
+lxc config device remove n1 myport8080
