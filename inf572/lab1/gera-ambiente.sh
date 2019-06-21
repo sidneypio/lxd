1cbcad52c26e3a40e0e2f38432f74236d6a884d9
Sidney Pio de Campos <sidneypio@gmail.com>
2019-06-21 16:41:28 -0300

    ajustes na organizacao e scripts do lab1

--- inf572/lab1/gera-ambiente.sh
+++ inf572/lab1/gera-ambiente.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-# gera o ambiente:
-# duas maquinas ligadas em um roteador 
-#
-#   A1 -------------------------- R --------------- B1
-#      eth1                  eth1   eth2       eth1
-#      10.10               10.100   20.100    20.10 
-#
+# gera o ambiente
 #
 echo "Criando redes "
-lxc network create redeAR ipv6.address=none ipv4.address=10.10.10.1/24 ipv4.nat=false ipv4.dhcp=false
-lxc network create redeBR ipv6.address=none ipv4.address=10.10.20.1/24 ipv4.nat=false ipv4.dhcp=false
+lxc network create redeAR ipv6.address=2001:db8:2018:A::1/64 ipv4.address=10.10.10.1/24 ipv4.nat=false ipv4.dhcp=false
+lxc network create redeBR ipv6.address=2001:db8:2018:B::1/64 ipv4.address=10.10.20.1/24 ipv4.nat=false ipv4.dhcp=false
 
 ### A1
-echo "Criando a primeira maquina"
+echo "Criando a maquina A1"
 lxc copy debianPadrao A1
-echo "Ligando interface eth1 na rede interna"
+echo "Ligando interface eth0 na rede interna"
 lxc network attach redeAR A1 eth0
 echo "Copiando configuracao de rede"
 lxc file push ./conf/A1/interfaces A1/etc/network/interfaces
 
+### A2
+echo "Criando a maquina A2"
+lxc copy debianPadrao A2
+echo "Ligando interface eth0 na rede interna"
+lxc network attach redeAR A2 eth0
+echo "Copiando configuracao de rede"
+lxc file push ./conf/A2/interfaces A2/etc/network/interfaces
+
 ### B
-echo "Criando a segunda maquina"
+echo "Criando a segunda B1"
 lxc copy debianPadrao B1
 echo "Ligando interface eth1 na rede interna"
 lxc network attach redeBR B1 eth0
@@ -28,7 +30,7 @@ echo "Copiando configuracao de rede"
 lxc file push ./conf/B1/interfaces B1/etc/network/interfaces 
 
 ### R
-echo "Criando roteador"
+echo "Criando roteador R"
 lxc copy debianPadrao R
 echo "Ligando interfaces"
 lxc network attach redeAR R eth1
@@ -42,4 +44,5 @@ lxc file push ./conf/R/rc.local R/etc/rc.local
 echo "Iniciando containers"
 lxc start R
 lxc start A1
+lxc start A2
 lxc start B1
--- inf572/lab1/remove-ambiente.sh
+++ inf572/lab1/remove-ambiente.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# remove o primeiro ambiente:
-# as duas maquinas e a rede
+# remove o ambiente:
 echo "Parando as maquinas"
 lxc exec A1 -- /sbin/poweroff
+lxc exec A2 -- /sbin/poweroff
 lxc exec B1 -- /sbin/poweroff
 lxc exec R -- /sbin/poweroff
 
 echo "Removendo as maquinas"
 lxc delete A1 
+lxc delete A2
 lxc delete B1 
 lxc delete R
 
