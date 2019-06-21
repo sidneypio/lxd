a09be8daefa8e2568e3540e40c85680a1d62de47
Sidney Pio de Campos <sidneypio@gmail.com>
2019-06-21 19:28:56 -0300

    ajustes nos scripts do lab2 e zona de tempo na imagem padrao do debian

--- cria-debianPadrao.sh
+++ cria-debianPadrao.sh
@@ -12,5 +12,8 @@ lxc exec debianPadrao -- /usr/bin/apt update
 echo "Instalando alguns pacotes uteis para a disciplina"
 lxc exec debianPadrao -- /usr/bin/apt install -y tcpdump net-tools inetutils-ping traceroute iptables htop bind9-host dnsutils rsyslog
 
+echo "Ajustando o timezone para America/Sao_Paulo"
+lxc exec debianPadrao -- timedatectl set-timezone "America/Sao_Paulo"
+
 echo "Desligando a maquina"
 lxc exec debianPadrao -- /sbin/poweroff
