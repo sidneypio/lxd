00b0b95cb62ed370aed6cc225a8b4733f2931c26
Sidney Pio de Campos <sidneypio@gmail.com>
2019-06-21 19:28:56 -0300

    ajustes nos scripts do lab2 e zona de tempo na imagem padrao do debian

--- inf572/lab2/testa-ambiente.sh
+++ inf572/lab2/testa-ambiente.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # testa o ambiente criado
 #
-echo "verificando conectidade a partir de A1"
-lxc exec A1 -- /bin/ping -c 3 10.10.10.11
-lxc exec A1 -- /bin/ping -c 3 10.10.10.100
-lxc exec A1 -- /bin/ping -c 3 10.10.20.100
-lxc exec A1 -- /bin/ping -c 3 10.10.20.10
-echo "verificando DNS"
-lxc exec A1 -- /usr/bin/host www.debian.org
+echo "verificando conectidade IPv4 a partir de A2"
+lxc exec A2 -- /bin/ping -c 3 10.10.10.10
+lxc exec A2 -- /bin/ping -c 3 10.10.10.100
+lxc exec A2 -- /bin/ping -c 3 10.10.20.100
+lxc exec A2 -- /bin/ping -c 3 10.10.20.10
+
+echo "verificando DNS a partir de A2"
+lxc exec A2 -- /usr/bin/host www.debian.org
