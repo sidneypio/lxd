06a788547982d175f90841bf2f8b9fcf0cd8e341
Sidney Pio de Campos <sidneypio@gmail.com>
2019-07-27 17:54:02 -0300

    ajustes no lab1

--- inf572/lab1/remove-ambiente.sh
+++ inf572/lab1/remove-ambiente.sh
@@ -6,6 +6,8 @@ lxc exec A2 -- /sbin/poweroff
 lxc exec B1 -- /sbin/poweroff
 lxc exec R -- /sbin/poweroff
 
+for count in {9..0}; do printf "\rAguardando 10 segundos: %02d" "$count"; sleep 1; done ; echo ""
+
 echo "Removendo as maquinas"
 lxc delete A1 
 lxc delete A2
